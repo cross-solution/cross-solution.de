051eb502e4b5a2496a6d1832ca1a58fdb67293be
Carsten Bleek <bleek@cross-solution.de>
2019-11-11 12:17:27 +0100

    removes second timeline from about page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MyLayout.vue'),
+    component: () => import('layouts/CrossLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: 'about', component: () => import('pages/about.vue') },
