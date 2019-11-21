d552a0e0a13fe173ca86b584ff668fdcd706ff8f
unknown <mewk@gmx.de>
2019-11-21 16:10:17 +0100

    Removes Paralax Container. Fixes #2

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -23,26 +23,6 @@
             v-bind="member"
           />
         </div>
-        <div class="col-md-12">
-          <q-separator
-            insert
-            color="secondary"
-          />
-          <h3>Paralax Example</h3>
-          <q-card class="my-card">
-            <q-parallax
-              src="https://cdn.quasar.dev/img/parallax1.jpg"
-              :height="150"
-            />
-
-            <q-card-section>
-              <div class="text-h6">Our Changing Planet</div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
-          </q-card>
-
-        </div>
-
         <div class="col-12">
           <q-separator
             insert
