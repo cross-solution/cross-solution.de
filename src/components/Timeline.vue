98e04f470247ca3c3602ee202dfa6828e26a3bc5
Carsten Bleek <bleek@cross-solution.de>
2019-11-30 18:00:05 +0100

    timeline changes

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="q-px-lg q-pb-md">
-    <q-timeline
-      :layout="layout"
-      color="secondary"
-    >
+    <h1>Timeline</h1>
+    <q-timeline :layout="layout" color="secondary">
       <q-timeline-entry heading>
         Timeline
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Heute"
-        subtitle="2019"
-        side="left"
-      >
+      <q-timeline-entry title="Heute" subtitle="2019" side="left">
         <div>
-          Relaunch Homepage auf Basis von <a
-            href="#"
-            @click="dialog = true"
-          >Quasar</a>. Stellenmarkte.com
+          Der Titel für das Jahr 2019 steht von nicht fest. Wir migrieren
+          unseren eigenen Mailstack auf Mailcow. Relaunch Homepage auf Basis von
+          <a href="#" @click="dialog = true">Quasar</a>. Stellenmarkte.com.
         </div>
       </q-timeline-entry>
 
@@ -28,109 +21,98 @@
         icon="delete"
       >
         <div>
-          Konzeption und Aufbau eines Crawlers für Stellenanzeigen. Aufgrund der Zusammenarbeit mit
-          Remote Workern können wir viel schneller aktuelle Technologien nutzbar machen.
+          Konzeption und Aufbau eines Crawlers für Stellenanzeigen. Aufgrund der
+          Zusammenarbeit mit Remote Workern können wir viel schneller aktuelle
+          Technologien nutzbar machen.
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="2017"
-        subtitle="2017"
-        side="left"
-      >
+      <q-timeline-entry title="Vue, Strapi, Quasar" subtitle="2017" side="left">
         <div>
-
+          Gitlab fasst ein Jahr vue
+          <a
+            href="https://about.gitlab.com/blog/2017/11/09/gitlab-vue-one-year-l"
+            >zuammen</a
+          >.
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="2016"
-        subtitle="2016"
-        side="left"
-      >
+      <q-timeline-entry title="Mail" subtitle="2016" side="right">
         <div>
-          Mailstack mit Dovecot, Postfix, Roundcube und VimBAdmin. Gitlab entscheidet sich für <a href="https://about.gitlab.com/blog/2016/10/20/why-we-chose-vue/">vue</a>
+          Wieder ein Jahr mit viel Administration. Schwerpunkt ist Mail.
+          Migration Dovecot in GSuite. Migration Cyrus zu Dovecot. Mailstack mit
+          Dovecot, Postfix, Roundcube und VimBAdmin. Gitlab entscheidet sich für
+          <a href="https://about.gitlab.com/blog/2016/10/20/why-we-chose-vue/"
+            >vue</a
+          >
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="2015"
-        subtitle="2015"
-        side="left"
-      >
+      <q-timeline-entry title="Telefonie" subtitle="2015" side="left">
         <div>
-          Asterisk Salesforce mit PhoneMondo. Freeswitch mit Kamailio, Snom Telefonen und einer redundanten Mitel TK.
+          2015 ist wieder geprägt durch Telefonie. Wir beschäftogen uns mit der
+          Integration von Asterisk in Salesforce mit PhoneMondo. Freeswitch mit
+          Kamailio, Snom Telefonen und einer redundanten Mitel TK begleitet uns
+          das ganze Jahr.
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Remote Working"
-        subtitle="2014"
-        side="left"
-      >
+      <q-timeline-entry title="Remote Working" subtitle="2014" side="right">
         <div>
-          Wir rekrutieren das erste mal per oDesk (später Upwork) und tauchen ein in die Welt des "Remote Workings". Wir
-          erkennen einen Zusammenhang zwischen Open Source und Arbeit.
+          Wir rekrutieren das erste mal per oDesk (später Upwork) und tauchen
+          ein in die Welt des "Remote Workings". Wir erkennen einen Zusammenhang
+          zwischen Open Source und Arbeit.
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="YAWIK"
-        subtitle="2013"
-        side="left"
-      >
+      <q-timeline-entry title="YAWIK" subtitle="2013" side="left">
         <div>
-          YAWIK wird gestartet.
+          YAWIK wird gestartet. Wir sind der Meinung, Open Source auch im HR
+          Bereich viel verbessern könnte
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry heading>November, 2017</q-timeline-entry>
-
-      <q-timeline-entry
-        title="Voip und Telefonie"
-        subtitle="2012"
-        side="left"
-      >
+      <q-timeline-entry title="Voip und Telefonie" subtitle="2012" side="right">
         <div>
-          Wir entdecken für uns <a href="https://www.proxmox.com/en/">Proxmox</a>. Von uns erst erstmal unbemerkt taucht
+          Wir entdecken für uns
+          <a href="https://www.proxmox.com/en/">Proxmox</a>. Von uns erst
+          erstmal unbemerkt taucht
           <a href="https://de.wikipedia.org/wiki/GitLab">Gitlab</a> auf.
         </div>
       </q-timeline-entry>
 
       <q-timeline-entry
-        title="Event Title"
+        title="Systemadministration"
         subtitle="2011"
-        side="right"
+        side="left"
       >
         <div>
           <ul>
             <li>Migration Cyrus auf Dovecot mit 1500 Postfächern.</li>
             <li>Integration Webapplikationen mit Facebook.</li>
-            <li>Unterstützung Migration auf NFS4 und Kerberos für eine Forschungseinrichtungen in Deutschland.</li>
-            <li>Freeswitch Support für eine Fluggesellschaft. Hier warten wir gespannt auf Gemeinschaft 4.0</li>
+            <li>
+              Unterstützung Migration auf NFS4 und Kerberos für eine
+              Forschungseinrichtungen in Deutschland.
+            </li>
+            <li>
+              Freeswitch Support für eine Fluggesellschaft. Hier warten wir
+              gespannt auf Gemeinschaft 4.0
+            </li>
             <li>Yate Installation für 250 Teilnehmern.</li>
           </ul>
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Voip"
-        subtitle="2010"
-        side="left"
-      >
+      <q-timeline-entry title="Voip" subtitle="2010" side="right">
         <div>
           <div class="">
-            Hylafax, Asterisk, SOGo. Redundante Telefonanlage für Unternehmen aus der
-            Solarbranche.
+            Hylafax, Asterisk, SOGo. Redundante Telefonanlage für Unternehmen
+            aus der Solarbranche.
           </div>
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Voip"
-        subtitle="2009"
-        side="right"
-      >
+      <q-timeline-entry title="Voip" subtitle="2009" side="left">
         <div>
           <div class="">LDAP, Asterisk</div>
         </div>
@@ -139,18 +121,19 @@
       <q-timeline-entry
         title="Infrastruktur"
         subtitle="2008"
-        side="left"
+        side="right"
         color="orange"
       >
         <div>
-          <div class="">Infrastruktur. <a href="https://de.wikipedia.org/wiki/GitHub">Github</a> erscheint</div>
+          Infrastruktur.
+          <a href="https://de.wikipedia.org/wiki/GitHub">Github</a> erscheint
         </div>
       </q-timeline-entry>
 
       <q-timeline-entry
         title="Webentwicklung"
         subtitle="2007"
-        side="right"
+        side="left"
         color="orange"
         icon="done_all"
       >
@@ -162,7 +145,7 @@
       <q-timeline-entry
         title="Intrastruktur"
         subtitle="2006"
-        side="left"
+        side="right"
         color="orange"
         icon="done_all"
       >
@@ -174,45 +157,48 @@
       <q-timeline-entry
         title="Voip"
         subtitle="2005"
-        side="right"
+        side="left"
         color="orange"
         icon="done_all"
       >
         <div>
-          Entwicklung des <a href="http://joboter.de">Joboter</a>. Alles noch mit SVN. Fax- und Voip Server Installationen. Alles noch in Hardwarbeit.
-          ISDN Karten, Kernel kompilieren. In diversen Veranstaltungen haben wir unsere Lösungen vorgestellt.
-          Von uns unbemerkt erscheint <a href="https://de.wikipedia.org/wiki/Git">GIT</a>.
+          Entwicklung des <a href="http://joboter.de">Joboter</a>. Alles noch
+          mit SVN. Fax- und Voip Server Installationen. Alles noch in
+          Hardwarbeit. ISDN Karten, Kernel kompilieren. In diversen
+          Veranstaltungen haben wir unsere Lösungen vorgestellt. Von uns
+          unbemerkt erscheint
+          <a href="https://de.wikipedia.org/wiki/Git">GIT</a>.
         </div>
       </q-timeline-entry>
 
       <q-timeline-entry
         title="Die Anfänge"
         subtitle="2004"
-        side="left"
+        side="right"
         color="#3b5998"
       >
         <div>
-          am Anfang machten wir alles. Entwicklung von Webseiten, Infrastrukturen. Ja sogar ein Design Konzept CI Briefpapier
-          und Visitenkarten. <a href="https://de.wikipedia.org/wiki/Facebook">Facebook</a> erscheint, und wächst viel schneller als wir.
+          am Anfang machten wir alles. Entwicklung von Webseiten,
+          Infrastrukturen. Ja sogar ein Design Konzept CI Briefpapier und
+          Visitenkarten.
+          <a href="https://de.wikipedia.org/wiki/Facebook">Facebook</a>
+          erscheint, und wächst viel schneller als wir.
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Start"
-        subtitle="2003"
-        side="right"
-      >
+      <q-timeline-entry title="Start" subtitle="2003" side="left">
         <div>
-          2003 startet CROSS Solution. Damals noch in der Großen Seestraße.
-          Ein Großteil unserer Erfahrung in der Architektur von Human Ressource Systemen für das
-          Online-Recruitment basiert auf sechs Jahren maßgeblicher Entwicklung des Jobpilot Stellenmarktes.
+          2003 startet CROSS Solution. Damals noch in der Großen Seestraße. Ein
+          Großteil unserer Erfahrung in der Architektur von Human Ressource
+          Systemen für das Online-Recruitment basiert auf sechs Jahren
+          maßgeblicher Entwicklung des Jobpilot Stellenmarktes.
         </div>
       </q-timeline-entry>
     </q-timeline>
   </div>
 </template>
 
-<script>
+<script lang="javascript">
 export default {
   data () {
     return {
@@ -221,7 +207,11 @@ export default {
   },
   computed: {
     layout () {
-      return this.$q.screen.lt.sm ? 'dense' : (this.$q.screen.lt.md ? 'comfortable' : 'loose')
+      return this.$q.screen.lt.sm
+        ? 'dense'
+        : this.$q.screen.lt.md
+          ? 'comfortable'
+          : 'loose'
     }
   }
 }
