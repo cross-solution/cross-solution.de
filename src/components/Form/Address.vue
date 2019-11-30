ac296d27eee837a2e2ebd77ec3a2a017a760da7a
Carsten Bleek <bleek@cross-solution.de>
2019-11-30 18:01:42 +0100

    moves Address into a component

---
+++ src/components/Form/Address.vue
@@ -0,0 +1,62 @@
+<!--
+just the Logo
+-->
+<template>
+  <div class="address">
+    <div class="row">
+      <div class="col-md-2 col-sm-6">
+        <q-select v-model="gender" :options="options" label="Anrede" />
+      </div>
+      <div class="col-md-10 col-sm-6" />
+      <div class="col-6">
+        <q-input filled v-model="firstname" label="Firstname" />
+      </div>
+      <div class="col-6">
+        <q-input filled v-model="lastname" label="Lastname" />
+      </div>
+      <div class="col-10">
+        <q-input filled v-model="street" label="Strasse" />
+      </div>
+      <div class="col-2">
+        <q-input filled v-model="housenumber" label="Hausnummer" />
+      </div>
+      <div class="col-2">
+        <q-input filled v-model="postalcode" label="Plz." />
+      </div>
+      <div class="col-10">
+        <q-input filled v-model="postalcode" label="Ort" />
+      </div>
+      <div class="col-6 col-xs-12">
+        <q-input filled v-model="email" label="Email" />
+      </div>
+      <div class="col-6 col-xs-12">
+        <q-input filled v-model="phone" label="Phone" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
+
+<script lang="javascript">
+
+export default {
+  name: 'Address',
+  data () {
+    return {
+      gender: '',
+      options: [
+        'Herr', 'Frau', 'Diverse'
+      ],
+      phone: '',
+      email: '',
+      postalcode: '',
+      city: '',
+      housenumber: '',
+      firstname: '',
+      lastname: '',
+      street: ''
+    }
+  }
+}
+</script>
--- src/pages/apply.vue
+++ src/pages/apply.vue
@@ -1,46 +1,94 @@
 <template>
   <q-page padding>
     <h1>Bewerben</h1>
-    <div class="row q-gatter-md">
+    <div>
+      So könnte ein Bewerbungsformular ausshen. Es besteht aus verschiedenen
+      Komponenten.
+    </div>
+
+    <div class="row">
       <h2 class="col-md-12">Persönliche Daten</h2>
-      <div class="col-md-2">
-        <q-select v-model="model" :options="gender" label="Anrede" />
-      </div>
-      <div class="col-md-10" />
-      <div class="col-md-6">
-        <q-input filled v-model="firstname" label="Firstname" />
-      </div>
-      <div class="col-md-6">
-        <q-input filled v-model="lastname" label="Lastname" />
-      </div>
-      <div class="col-md-10">
-        <q-input filled v-model="street" label="Strasse" />
+
+      <div class="col-md-10 col-sm-12">
+        <div class="row q-gutter-xs">
+          <y-address />
+        </div>
       </div>
       <div class="col-md-2">
-        <q-input filled v-model="housenumber" label="Hausnummer" />
-      </div>
-      <div class="col-md-2">
-        <q-input filled v-model="postalcode" label="Plz." />
-      </div>
-      <div class="col-md-10">
-        <q-input filled v-model="postalcode" label="Ort" />
-      </div>
-      <div class="col-md-6">
-        <q-input filled v-model="email" label="Email" />
-      </div>
-      <div class="col-md-6">
-        <q-input filled v-model="phone" label="Phone" />
+        <q-uploader
+          label="Bild Upload"
+          url="http://localhost:4444/upload"
+          style="max-width: 300px"
+        />
       </div>
     </div>
     <h2>Anschreiben</h2>
     <q-editor v-model="editor" min-height="5rem" placeholder="Anschreiben" />
     <h2>Dokumente</h2>
-    <h3>Blindkopie</h3>
-    <h3>Datenschutzbestimmungen</h3>
+    <q-uploader
+      url="http://localhost:4444/upload"
+      label="Anlagen zur Bewerbung"
+      multiple
+      style="max-width: 300px"
+    />
+    <h2>Sonstige Angaben</h2>
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input
+        label="frühester Eintrittstermin"
+        filled
+        v-model="date"
+        hint="wann können Sie anfangen?"
+        mask="date"
+        :rules="['date']"
+      >
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy
+              ref="qDateProxy"
+              transition-show="scale"
+              transition-hide="scale"
+            >
+              <q-date v-model="date" @input="() => $refs.qDateProxy.hide()" />
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+    </div>
+    <q-separator />
+
+    <q-item tag="label" v-ripple>
+      <q-item-section avatar top>
+        <q-checkbox v-model="cc" val="primary" color="primary" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Blindkopie</q-item-label>
+        <q-item-label caption>
+          Bitte senden Sie mir meine übermittelten Daten per CC.
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item tag="label" v-ripple>
+      <q-item-section avatar top>
+        <q-checkbox v-model="privacy" val="primary" color="primary" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Datenschuz</q-item-label>
+        <q-item-label caption>
+          ich habe die Datenschutzbestimmungen gelesen und akzeptiere Sie
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <q-btn color="info" icon-right="cancel" label="Voransicht" />
+    <q-btn color="warning" icon-right="cancel" label="Abbrechen" />
+    <q-btn color="primary" icon-right="send" label="Berwerbung absenden" />
   </q-page>
 </template>
 
 <script lang="javascript">
+
+import YAddress from '../components/Form/Address.vue'
+
 export default {
   name: 'Apply',
   data () {
@@ -49,8 +97,14 @@ export default {
       gender: [
         'Herr', 'Frau', 'Diverse'
       ],
-      editor: 'Ihr Anschreiben'
+      editor: 'Ihr Anschreiben',
+      privacy: false,
+      cc: false,
+      date: ''
     }
+  },
+  components: {
+    YAddress
   }
 }
 </script>
