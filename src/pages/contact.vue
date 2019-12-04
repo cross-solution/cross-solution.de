dcdf0c73ef3e5883b2e3ce089e5ec0fb024113e1
Carsten Bleek <bleek@cross-solution.de>
2019-12-04 20:43:11 +0100

    adds name and email to contact page

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,8 +1,14 @@
 <template>
   <q-page padding>
-    <h1>Kontakt</h1>
-    Sie können uns direkt per Email kontaktieren oder nachfolgendes Kontaktformular verwenden. Vertrauliche Informationen können Sie uns verschlüsselt schicken (siehe www.gnupg.org). Unser öffenlicher Schlüssel ist hier verfügbar (Id: 0x8AD12510, Fingerprint: 40FE 2824 60BA BDE2 247E 919A DD27 60F5 8AD1 2510). Wir werden uns kurzfristig mit Ihnen in Verbindung setzen.
-
+    <h1>Get in touch</h1>
+    <div>
+      Sie können uns direkt per Email kontaktieren oder nachfolgendes
+      Kontaktformular verwenden. Vertrauliche Informationen können Sie uns
+      verschlüsselt schicken (siehe www.gnupg.org). Unser öffenlicher Schlüssel
+      ist hier verfügbar (Id: 0x8AD12510, Fingerprint: 40FE 2824 60BA BDE2 247E
+      919A DD27 60F5 8AD1 2510). Wir werden uns kurzfristig mit Ihnen in
+      Verbindung setzen.
+    </div>
     <div class="q-pa-md q-gutter-y-sm">
       <form
         autocorrect="off"
@@ -10,37 +16,27 @@
         autocomplete="off"
         spellcheck="true"
       >
-        <q-editor
-          v-model="editor"
-          min-height="5rem"
-        />
-        <q-card
-          flat
-          bordered
-        >
-          <q-card-section>
-            <pre style="white-space: pre-line">{{ editor }}</pre>
-          </q-card-section>
-        </q-card>
-        <q-card
-          flat
-          bordered
-        >
-          <q-card-section v-html="editor" />
-        </q-card>
+        <div>
+          <q-input v-model="name" label="Name" />
+          <q-input v-model="email" label="Email" />
+          <q-editor
+            v-model="editor"
+            min-height="5rem"
+            aria-placeholder="Ihre Nachricht"
+          />
+          <q-btn color="primary" icon-right="send" label="Absenden" />
+        </div>
       </form>
     </div>
-
   </q-page>
-
 </template>
 
-<script>
+<script lang="javascript">
 export default {
   name: 'Contact',
   data () {
     return {
-      editor: 'What you see is <b>what</b> you get.'
+      editor: ''
     }
   }
 }
