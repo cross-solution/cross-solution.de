9867dfeebbd1e975c35bebb3e033ab019f97e53f
Carsten Bleek <bleek@cross-solution.de>
2019-12-19 10:10:46 +0100

    removes modalbox with AGB

--- src/pages/post-a-job.vue
+++ src/pages/post-a-job.vue
@@ -202,51 +202,10 @@
           <q-item-section>
             <q-item-label>AGB</q-item-label>
             <q-item-label caption>
-              ich habe die
-              <q-btn
-                label="AGBs"
-                color="primary"
-                @click="fixed = true"
-              /> gelesen und akzeptiere Sie
+              ich habe die AGBs gelesen und akzeptiere Sie
             </q-item-label>
           </q-item-section>
         </q-item>
-        <q-dialog v-model="fixed">
-          <q-card>
-            <q-card-section>
-              <div class="text-h6">Terms of Agreement</div>
-            </q-card-section>
-
-            <q-separator />
-
-            <q-card-section
-              style="max-height: 50vh"
-              class="scroll"
-            >
-              <p
-                v-for="n in 15"
-                :key="n"
-              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
-            </q-card-section>
-
-            <q-separator />
-
-            <q-card-actions align="right">
-              <q-btn
-                flat
-                label="Decline"
-                color="primary"
-                v-close-popup
-              />
-              <q-btn
-                flat
-                label="Accept"
-                color="primary"
-                v-close-popup
-              />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
       </q-step>
 
       <template v-slot:navigation>
