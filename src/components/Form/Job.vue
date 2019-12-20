1ee16a0fb13d03ec8432e7bce39a33b5dfa6a5c4
unknown <mewk@gmx.de>
2019-12-20 15:55:38 +0100

    PostJob changes / adds new images

--- src/components/Form/Job.vue
+++ src/components/Form/Job.vue
@@ -1,27 +1,35 @@
 <template>
-  <q-splitter v-model="splitterModel" style="height: 400px;">
+  <q-splitter v-model="splitterModel" style="height: 500px">
     <template v-slot:before>
       <form @change="emitJobDescription">
         <q-card>
           <q-card-section>
             <div class="job">
               <div class="row q-col-gutter-sm">
-                <y-photo-upload class="col-2"
-                color="transparent"
-                uploader-style="background: url(/statics/HeaderUpload.png) center no-repeat;  padding: 0px; border: 3px dashed gray;"
-                v-model="job.organizationLogo"
-              />
-              <div class="col-10">
-                <h3>{{job.organization}}</h3>
-                <h1>{{ job.title }}</h1>
+                <y-photo-upload
+                  class="col-3"
+                  color="transparent"
+                  minCanvasWidth="5rem"
+                  text-color="white"
+                  uploader-style="background: url(/statics/PhotoUpload.png) center no-repeat; color: #fff !important; padding: 0px; border: 2px dashed #ccc;"
+                  v-model="job.organizationLogo"
+                />
+                <div class="col-9">
+                  <h3>
+                    {{ job.organization }}
+                  </h3>
+                  <h1>
+                    {{ job.title }}
+                  </h1>
+                </div>
               </div>
-            </div>
               <y-photo-upload
                 color="transparent"
-                uploader-style="background: url(/statics/HeaderUpload.png) center no-repeat;  padding: 0px; border: 3px dashed gray;"
+                uploader-style="background: url(/statics/HeaderUpload.jpg) center no-repeat;  padding: 5%; border: 2px dashed #ccc;"
                 v-model="job.headerImage"
               />
               <q-editor
+                style="margin-top: 25px;"
                 v-model="job.description"
                 :dense="$q.screen.gt.md"
                 min-height="4rem"
@@ -40,15 +48,23 @@
                 <q-editor v-model="job.tasksText" min-height="5rem" />
               </div>
               <div class="col-md-6">
-                <q-input class="text-h6" borderless v-model="job.qualificationsTitle" />
+                <q-input class="text-h6" v-model="job.qualificationsTitle" />
                 <q-editor v-model="job.qualificationsText" min-height="5rem" />
               </div>
               <div class="col-md-6">
-                <q-input class="text-h6" borderless v-model="job.benefitsTitle" />
+                <q-input
+                  class="text-h6"
+                  borderless
+                  v-model="job.benefitsTitle"
+                />
                 <q-editor v-model="job.benefitsText" min-height="5rem" />
               </div>
               <div class="col-md-6">
-                <q-input class="text-h6" borderless v-model="job.contactTitle" />
+                <q-input
+                  class="text-h6"
+                  borderless
+                  v-model="job.contactTitle"
+                />
                 <q-editor v-model="job.contactText" min-height="5rem" />
               </div>
             </div>
@@ -58,25 +74,68 @@
     </template>
 
     <template v-slot:after>
-      <q-card class="my-card">
-        <h3>{{job.organization}}</h3>
-        <h1>{{ job.title }}</h1>
-        <img :src="job.headerImage"/>
-        <q-card-section>
-        <div>
-          <p v-html="job.description"></p>
-        </div>
-        <div>{{ job.searching }}</div>
+      <q-card>
+        <div class="row q-col-gutter-sm">
+          <div class="col-3">
+            <div class="LogoImage">
+              <img :src="job.headerImage" />
+            </div>
+          </div>
+          <div class="col-9">
+            <h3 class="preview">{{ job.organization }}</h3>
+            <h1 class="previewJobTitle">{{ job.title }}</h1>
+          </div>
+          <div class="HeaderImage col-12">
+            <img :src="job.headerImage" />
+          </div>
 
-          <h6>{{ job.contactTitle }}</h6>
-          <div v-html="job.contactText"></div>
-        </q-card-section>
+          <q-card-section>
+            <div>
+              <p v-html="job.description"></p>
+            </div>
+            <div>{{ job.searching }}</div>
+
+            <h6>{{ job.contactTitle }}</h6>
+            <div v-html="job.contactText"></div>
+          </q-card-section>
+        </div>
       </q-card>
     </template>
   </q-splitter>
 </template>
 
-<style lang="scss"></style>
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+  font-weight: 400;
+}
+h3 {
+  font-size: 1.5rem;
+  color: #555;
+}
+.preview {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.previewJobTitle {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.HeaderImage {
+  background: url(/statics/HeaderUpload.jpg) center no-repeat;
+  padding: 5%;
+}
+.LogoImage {
+  background: url(/statics/PhotoUpload.png) center no-repeat;
+  color: #fff !important;
+  padding: 0px;
+}
+.q-field__native {
+  border: 1px solid #eee;
+  background: #eee !important;
+  padding: 6px;
+}
+</style>
 
 <script lang="javascript">
 import YPhotoUpload from './PhotoUpload.vue'
