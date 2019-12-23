ff4d168882fe7d97afe950f658bec2e10178cdbf
unknown <mewk@gmx.de>
2019-12-23 12:47:07 +0100

    layout changes => padding out

--- src/pages/digital-change.vue
+++ src/pages/digital-change.vue
@@ -2,50 +2,24 @@
   <q-page padding>
     <h1>Digitaler Wandel</h1>
     <p>
-      Die Welt verändert sich. Das ist nichts Neues. Sie hat sich schon immer verändert.
-      Sie verändert sich nur schneller.
+      Die Welt verändert sich. Das ist nichts Neues. Sie hat sich schon immer
+      verändert. Sie verändert sich nur schneller.
     </p>
     <p>
       Wir sehen einen Zusammenhang zwischen Open Source und Arbeit. Und wir
       glauben, dass man einen Vorteil hat, wenn man dies erkennt.
     </p>
     <div class="q-pa-md q-gutter-sm">
-      <q-btn
-        label="YAWIK Demo"
-        color="primary"
-        @click="layout = true"
-      />
+      <q-btn label="YAWIK Demo" color="primary" @click="layout = true" />
 
       <q-dialog v-model="layout">
-        <q-layout
-          view="Lhh lpR fff"
-          container
-          class="bg-white"
-        >
+        <q-layout view="Lhh lpR fff" container class="bg-white">
           <q-header class="bg-primary">
             <q-toolbar>
-              <q-btn
-                flat
-                @click="drawer = !drawer"
-                round
-                dense
-                icon="menu"
-              />
+              <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
               <q-toolbar-title>YAWIK Demo</q-toolbar-title>
-              <q-btn
-                flat
-                @click="drawerR = !drawerR"
-                round
-                dense
-                icon="menu"
-              />
-              <q-btn
-                flat
-                v-close-popup
-                round
-                dense
-                icon="close"
-              />
+              <q-btn flat @click="drawerR = !drawerR" round dense icon="menu" />
+              <q-btn flat v-close-popup round dense icon="close" />
             </q-toolbar>
           </q-header>
 
@@ -55,35 +29,8 @@
             </q-toolbar>
           </q-footer>
 
-          <q-drawer
-            bordered
-            v-model="drawer"
-            :width="200"
-            :breakpoint="600"
-            content-class="bg-grey-3 q-pa-sm"
-          >
-            <div
-              v-for="n in 50"
-              :key="n"
-            >Drawer {{ n }} / 50</div>
-          </q-drawer>
-
-          <q-drawer
-            side="right"
-            bordered
-            v-model="drawerR"
-            :width="200"
-            :breakpoint="300"
-            content-class="bg-grey-3 q-pa-sm"
-          >
-            <div
-              v-for="n in 50"
-              :key="n"
-            >Drawer {{ n }} / 50</div>
-          </q-drawer>
-
           <q-page-container>
-            <q-page padding>
+            <q-page>
               <jobsearch />
             </q-page>
           </q-page-container>
@@ -93,13 +40,12 @@
   </q-page>
 </template>
 
-<script>
-
+<script lang="javascript">
 import Jobsearch from '../components/Jobsearch.vue'
 
 export default {
   meta: {
-    'title': 'Der digitale Wandel'
+    title: 'Der digitale Wandel'
   },
   name: 'DigitalChange',
   data () {
@@ -110,7 +56,8 @@ export default {
       drawer: false,
       drawerR: false,
 
-      lorem: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, ratione eum minus fuga, quasi dicta facilis corporis magnam, suscipit at quo nostrum!'
+      lorem:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, ratione eum minus fuga, quasi dicta facilis corporis magnam, suscipit at quo nostrum!'
     }
   },
   computed: {
