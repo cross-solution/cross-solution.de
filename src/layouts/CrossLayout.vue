294c8cadcba30568ae39eed82e2ec8fbeb08c788
Carsten Bleek <bleek@cross-solution.de>
2019-12-27 11:28:17 +0100

    renames property. #36

--- src/layouts/CrossLayout.vue
+++ src/layouts/CrossLayout.vue
@@ -131,7 +131,7 @@
         <q-route-tab name="privacy" to="/privacy" label="Datenschutz" />
         <q-route-tab name="contact" to="/contact" label="Kontakt" />
       </q-tabs>
-      <login-info :uri="loginUri" />
+      <login-info :host="loginUri" />
       <a href="https://github.com/cross-solution">
         <q-icon name="fab fa-github" size="lg" />
       </a>
