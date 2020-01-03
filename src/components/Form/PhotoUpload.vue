9a2c5b1f9d4fcdc1dcd1568623b6d80431e57881
Carsten Bleek <bleek@cross-solution.de>
2020-01-03 22:44:55 +0100

    formats source

--- src/components/Form/PhotoUpload.vue
+++ src/components/Form/PhotoUpload.vue
@@ -8,7 +8,6 @@
       :multiple="multiple"
       color="light-blue-1"
       text-color="grey-9"
-      ref="input"
       field-name="image"
       :max-total-size="maxTotalSize"
       @added="setImage"
@@ -17,85 +16,87 @@
         <q-card flat>
           <q-spinner v-if="scope.isUploading" class="q-uploader__spinner" />
           <div class="text-subtitle2 flex no-wrap q-gutter-xl">
-           <div class="q-uploader__title">{{textSelectFile}}
-            <div class="q-uploader__subtitle">
-              {{ scope.uploadSizeLabel }} /
-              {{ scope.uploadProgressLabel }}
-            </div>
+            <div class="q-uploader__title">
+              {{textSelectFile}}
+              <div class="q-uploader__subtitle">
+                {{ scope.uploadSizeLabel }} /
+                {{ scope.uploadProgressLabel }}
+              </div>
             </div>
             <q-btn v-if="scope.canAddFiles" type="a" icon="add_box" round dense flat>
-            <q-uploader-add-trigger />
-            <q-tooltip>{{textToolTip}}</q-tooltip>
-          </q-btn>
-          <q-btn v-if="scope.canUpload" icon="cloud_upload" @click="scope.upload" round dense flat>
-            <q-tooltip>Upload Files</q-tooltip>
-          </q-btn>
+              <q-uploader-add-trigger />
+              <q-tooltip>{{textToolTip}}</q-tooltip>
+            </q-btn>
+            <q-btn
+              v-if="scope.canUpload"
+              icon="cloud_upload"
+              @click="scope.upload"
+              round
+              dense
+              flat
+            >
+              <q-tooltip>Upload Files</q-tooltip>
+            </q-btn>
           </div>
         </q-card>
       </template>
 
       <template v-slot:list="scope">
-        <q-card flat style="padding:0; border: 2px dashed #ccc;">
-          <div v-if="scope.queuedFiles.length == 0">
-            <q-img
-              style="width: 100%; min-height: 7rem"
-              :src="defaultImage"
-            />
-          </div>
-          <div v-else>
-            <div v-for="file in scope.files" :key="file.name">
-              <q-img :src="file.__img.src" style="width: 100%; min-height: 7rem">
-                <q-btn icon="delete" @click="scope.removeQueuedFiles" round dense flat>
-                  <q-tooltip>{{$t('Clear')}}</q-tooltip>
-                </q-btn>
-
-                <q-spinner v-if="scope.isUploading" class="q-uploader__spinner" />
-                <div class="text-subtitle2 absolute-bottom text-center">
-                  <div class="q-uploader__subtitle">
-                    {{ scope.uploadSizeLabel }} /
-                    {{ scope.uploadProgressLabel }}
-                  </div>
-                </div>
-                <q-btn
-                  v-if="scope.canUpload"
-                  icon="cloud_upload"
-                  @click="scope.upload"
-                  round
-                  dense
-                  flat
-                >
-                  <q-tooltip>Upload</q-tooltip>
-                </q-btn>
-
-                <q-btn v-if="scope.isUploading" icon="clear" @click="scope.abort" round dense flat>
-                  <q-tooltip>Abort Upload</q-tooltip>
-                </q-btn>
-                <q-btn round icon="edit" dense flat @click="cropperDialog = true">
-                  <q-tooltip>Edit</q-tooltip>
-                </q-btn>
-              </q-img>
+        <div>
+          <q-card flat style="padding:0; border: 2px dashed #ccc;">
+            <div v-if="scope.queuedFiles.length == 0">
+              <q-img style="width: 100%; min-height: 7rem" :src="defaultImage" />
             </div>
-          </div>
-        </q-card>
+            <div v-else>
+              <div v-for="file in scope.files" :key="file.name">
+                <q-img :src="file.__img.src" style="width: 100%; min-height: 7rem">
+                  <q-btn icon="delete" @click="scope.removeQueuedFiles" round dense flat>
+                    <q-tooltip>{{$t('Clear')}}</q-tooltip>
+                  </q-btn>
+
+                  <q-spinner v-if="scope.isUploading" class="q-uploader__spinner" />
+                  <div class="text-subtitle2 absolute-bottom text-center">
+                    <div class="q-uploader__subtitle">
+                      {{ scope.uploadSizeLabel }} /
+                      {{ scope.uploadProgressLabel }}
+                    </div>
+                  </div>
+                  <q-btn
+                    v-if="scope.canUpload"
+                    icon="cloud_upload"
+                    @click="scope.upload"
+                    round
+                    dense
+                    flat
+                  >
+                    <q-tooltip>Upload</q-tooltip>
+                  </q-btn>
+                </q-img>
+              </div>
+              <q-dialog seamless v-model="cropperDialog">
+                <q-card>
+                  <q-card-section>
+                    <vue-cropper
+                      ref="input"
+                      :src="imgSrc"
+                      alt="Source Image"
+                      :cropmove="cropImage"
+                    ></vue-cropper>
+                  </q-card-section>
+                  <q-separator />
+                  <q-card-actions>
+                    <q-btn icon="fas fa-crop-alt" flat color="primary" @click="cropImage"></q-btn>
+                    <q-btn icon="redo" flat color="primary" @click="rotate(-90)"></q-btn>
+                    <q-btn icon="undo" flat color="primary" @click="rotate(90)"></q-btn>
+                    <q-btn flat color="primary" v-close-popup>OK</q-btn>
+                  </q-card-actions>
+                </q-card>
+              </q-dialog>
+            </div>
+          </q-card>
+        </div>
       </template>
     </q-uploader>
-
-    <q-dialog seamless v-model="cropperDialog">
-      <q-card>
-        <q-card-section>
-          <vue-cropper ref="cropper" :src="imgSrc" alt="Source Image" :cropmove="cropImage"></vue-cropper>
-        </q-card-section>
-
-        <q-separator />
-
-        <q-card-actions>
-          <q-btn icon="fas fa-crop-alt" flat color="primary" @click="cropImage"></q-btn>
-          <q-btn icon="redo" flat color="primary" @click="rotate(-90)"></q-btn>
-          <q-btn icon="undo" flat color="primary" @click="rotate(90)"></q-btn>
-          <q-btn flat color="primary" v-close-popup>OK</q-btn>
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
   </div>
 </template>
 
@@ -110,8 +111,8 @@ export default {
     return {
       maxTotalSize: 2048000,
       cropperDialog: false,
-      imgSrc: '/statics/LogoUpload.png',
-      cropImg: '',
+      imgSrc: null,
+      cropImg: null,
       image: null
     }
   },
@@ -147,6 +148,7 @@ export default {
       this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL()
     },
     setImage (files) {
+      console.log('Files: ' + files)
       const file = files[0]
       if (file.type.indexOf('image/') === -1) {
         alert('Please select an image file')
@@ -168,14 +170,22 @@ export default {
   },
   computed: {
     textSelectFile () {
-      console.log('compute textSelectFile: ' + this.uploadType)
       if (!this.uploadType) {
         return this.$t('select file', { do: this.$t('Select'), what: 'File' })
       }
-      return this.$t('select file', { do: this.$t('Select'), what: this.$t(this.uploadType) })
+      return this.$t('select file', {
+        do: this.$t('Select'),
+        what: this.$t(this.uploadType)
+      })
     },
     textToolTip () {
-      return ''
+      if (!this.uploadType) {
+        return this.$t('select file', { do: this.$t('Select'), what: 'File' })
+      }
+      return this.$t('select file', {
+        do: this.$t('Select'),
+        what: this.$t(this.uploadType)
+      })
     }
   }
 }
