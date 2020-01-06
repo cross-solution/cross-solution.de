437416cbaf228fcee37c020c88e536562efe15d4
Carsten Bleek <bleek@cross-solution.de>
2020-01-06 19:41:37 +0100

    adds Google and Github APP ID

--- src/boot/hello.js
+++ src/boot/hello.js
@@ -3,9 +3,9 @@ import hello from 'hellojs'
 export default ({ Vue }) => {
   hello.init({
     facebook: '124497304281524',
-    google: '',
+    google: '363285585170-gtb525id1tir90ab2ursh5hqe8bvm9l3.apps.googleusercontent.com',
     linkedin: '7806tax4rugtc4',
-    github: '',
+    github: 'f8f1609e2fa8d63fe6ff',
     dropbox: 'vzem154zcd5f7um'
   }, { redirect_uri: '/cv' })
   Vue.prototype.$hello = hello
