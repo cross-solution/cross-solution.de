185b9318ecf12c01cb48aec25575bd9ab7fac6ee
Carsten Bleek <bleek@cross-solution.de>
2020-01-07 11:40:42 +0100

    moves right drawer into a component

--- src/layouts/CV.vue
+++ src/layouts/CV.vue
@@ -111,98 +111,7 @@
         </q-img>
       </q-drawer>
 
-    <q-drawer v-model="right" side="right" overlay bordered>
-      <div class="text-secondary q-pb-md">
-        <q-btn
-          flat
-          color="primary"
-          class="full-width lt-md"
-          :label="$t('Digital Change')"
-          icon="calendar"
-          align="arround"
-          to="/digital-change"
-        />
-        <q-btn
-          flat
-          color="primary"
-          class="full-width lt-md"
-          label="Open Source"
-          icon="calendar"
-          align="arround"
-          to="/open-source"
-        />
-        <q-btn
-          flat
-          color="primary"
-          class="full-width lt-md"
-          :label="$t('About Us')"
-          icon="calendar"
-          align="arround"
-          to="/about"
-        />
-        <q-btn
-          flat
-          color="primary"
-          class="full-width lt-md"
-          label="Timeline"
-          icon="calendar"
-          align="arround"
-          to="/timeline"
-        />
-        <q-separator />
-        <h4 class="text-center">Yawik Demo</h4>
-        <q-btn
-          flat
-          color="primary"
-          class="full-width"
-          :label="$t('Search Jobs')"
-          align="arround"
-          icon="search"
-          to="/jobs"
-        />
-        <q-separator />
-        <q-btn
-          flat
-          color="primary"
-          class="full-width"
-          :label="$t('Post a Job')"
-          align="arround"
-          icon="edit"
-          to="/jobpost"
-        />
-        <q-separator />
-        <q-btn
-          flat
-          color="primary"
-          class="full-width"
-          :label="$t('Apply')"
-          align="arround"
-          icon="mail"
-          to="/apply"
-        />
-                <q-separator />
-        <q-btn
-          flat
-          color="primary"
-          class="full-width"
-          :label="$t('CV')"
-          align="arround"
-          icon="school"
-          to="/cv"
-        />
-        <q-separator />
-        <div class="q-gutter-md">
-          <p class="text-grey-9 q-pa-md">
-            Die Stellenangebote werden zur Verfügung gestellt von
-            <a href="https://www.stellenmarkt.com">Stellenmarkt.com</a>. Wie die
-            Integration im Detail geschieht, sehen Sie auf
-            <a href="https://github.com/cross-solution/cross-solution.de"
-              >Guthub</a
-            >.
-          </p>
-        </div>
-      </div>
-    </q-drawer>
+    <drawer-right :right="right"/>
 
     <q-page-container>
       <router-view />
@@ -217,6 +126,7 @@ import LoginInfo from '../components/LoginInfo.vue'
 import Logo from '../components/Logo.vue'
 import LanguageSwitch from '../components/LanguageSwitch'
 import LayoutFooter from './parts/footer.vue'
+import DrawerRight from './parts/right.vue'
 
 export default {
   data () {
@@ -231,7 +141,8 @@ export default {
     Logo,
     LoginInfo,
     LanguageSwitch,
-    LayoutFooter
+    LayoutFooter,
+    DrawerRight
   }
 }
 </script>
---
+++ src/layouts/parts/right.vue
@@ -0,0 +1,100 @@
+<template>
+    <q-drawer v-model="right" side="right" overlay bordered>
+      <div class="text-secondary q-pb-md">
+        <q-btn
+          flat
+          color="primary"
+          class="full-width lt-md"
+          :label="$t('Digital Change')"
+          icon="calendar"
+          align="arround"
+          to="/digital-change"
+        />
+        <q-btn
+          flat
+          color="primary"
+          class="full-width lt-md"
+          label="Open Source"
+          icon="calendar"
+          align="arround"
+          to="/open-source"
+        />
+        <q-btn
+          flat
+          color="primary"
+          class="full-width lt-md"
+          :label="$t('About Us')"
+          icon="calendar"
+          align="arround"
+          to="/about"
+        />
+        <q-btn
+          flat
+          color="primary"
+          class="full-width lt-md"
+          label="Timeline"
+          icon="calendar"
+          align="arround"
+          to="/timeline"
+        />
+        <q-separator />
+        <h4 class="text-center">Yawik Demo</h4>
+        <q-btn
+          flat
+          color="primary"
+          class="full-width"
+          :label="$t('Search Jobs')"
+          align="arround"
+          icon="search"
+          to="/jobs"
+        />
+        <q-separator />
+        <q-btn
+          flat
+          color="primary"
+          class="full-width"
+          :label="$t('Post a Job')"
+          align="arround"
+          icon="edit"
+          to="/jobpost"
+        />
+        <q-separator />
+        <q-btn
+          flat
+          color="primary"
+          class="full-width"
+          :label="$t('Apply')"
+          align="arround"
+          icon="mail"
+          to="/apply"
+        />
+                <q-separator />
+        <q-btn
+          flat
+          color="primary"
+          class="full-width"
+          :label="$t('CV')"
+          align="arround"
+          icon="school"
+          to="/cv"
+        />
+        <q-separator />
+        <div class="q-gutter-md">
+          <p class="text-grey-9 q-pa-md">
+            Die Stellenangebote werden zur Verfügung gestellt von
+            <a href="https://www.stellenmarkt.com">Stellenmarkt.com</a>. Wie die
+            Integration im Detail geschieht, sehen Sie auf
+            <a href="https://github.com/cross-solution/cross-solution.de"
+              >Github</a
+            >.
+          </p>
+        </div>
+      </div>
+    </q-drawer>
+</template>
+
+<script lang="javascrip">
+export default {
+  props: ['right']
+}
+</script>
