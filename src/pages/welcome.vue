87d92bf17de0afcfcac5cc504152cfffc7d0ae77
Carsten Bleek <bleek@cross-solution.de>
2020-01-09 19:10:17 +0100

    adds welcome page

---
+++ src/pages/welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <h1>Welcome</h1>
+    <h1>@</h1>
+    <logo/>
+  </div>
+</template>
+
+<script>
+import Logo from '../components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
