8e0eec4168a92fbbcb86ce9797ec0a511ef57e5d
Carsten Bleek <bleek@cross-solution.de>
2020-01-27 14:34:26 +0100

    replaces backticks by quotes

--- src/boot/notifications.js
+++ src/boot/notifications.js
@@ -18,7 +18,7 @@ function gdpr () {
   var expire = today + 1000 * 60 * 60 * 24 * 365 // milliseconds = 1 year
 
   Notify.create({
-    message: `Unsere Webseite verwendet Cookies.`,
+    message: 'Unsere Webseite verwendet Cookies.',
     classes: 'doc-gdpr',
     timeout: 0,
     position: 'bottom-right',
