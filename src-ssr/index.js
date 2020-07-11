b759a6dd70efda95e33c02b54df0eb6260fec33d
Carsten Bleek <bleek@cross-solution.de>
2020-07-11 10:21:08 +0200

    update generated code

--- src-ssr/index.js
+++ src-ssr/index.js
@@ -2,24 +2,22 @@
  * This file runs in a Node context (it's NOT transpiled by Babel), so use only
  * the ES6 features that are supported by your Node version. https://node.green/
  *
- * All content of this folder will be copied as is to the output folder. So only import:
- *  1. node_modules (and yarn/npm install dependencies -- NOT to devDependecies though)
- *  2. create files in this folder and import only those with the relative path
+ * WARNING!
+ * If you import anything from node_modules, then make sure that the package is specified
+ * in package.json > dependencies and NOT in devDependencies
  *
  * Note: This file is used only for PRODUCTION. It is not picked up while in dev mode.
  *   If you are looking to add common DEV & PROD logic to the express app, then use
  *   "src-ssr/extension.js"
  */
 
-const
-  express = require('express'),
-  compression = require('compression')
+const express = require('express')
+const compression = require('compression')
 
-const
-  ssr = require('../ssr'),
-  extension = require('./extension'),
-  app = express(),
-  port = process.env.PORT || 3000
+const ssr = require('quasar-ssr')
+const extension = require('./extension')
+const app = express()
+const port = process.env.PORT || 3000
 
 const serve = (path, cache) => express.static(ssr.resolveWWW(path), {
   maxAge: cache ? 1000 * 60 * 60 * 24 * 30 : 0
