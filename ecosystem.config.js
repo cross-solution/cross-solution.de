4fca6dd988644a4e8a44a024fffb30cb7c492a80
Carsten Bleek <bleek@cross-solution.de>
2022-02-11 15:00:28 +0100

    deploy using pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ apps : [{
     script: 'yarn',
     args: 'start',
     env: {
-      NODE_ENV: "production",
-      PORT: 3000
+      NODE_ENV: "develop",
+      PORT: 8080
     },
     env_production: {
       NODE_ENV: "production",
@@ -20,8 +20,8 @@ apps : [{
       ref  : 'origin/main',
       repo : 'https://gitlab.com/cross-solution/homepage',
       path : '/home/yawik/cross-solution.de',
-      'pre-deploy-local': 'echo "pre-deploy-local"',
-      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production -- --port 3000',
+      'pre-deploy-local': 'rsync -ra --exclude=node_modules --exclude=frankfurt-im-nebel.jpg --delete dist/ssr/ yawik@cross-solution.de:cross-solution/source/dist/ssr/',
+      'post-deploy' : 'pm2 reload ecosystem.config.js --env production -- --port 3000',
       'pre-setup': 'pm2 ps'
     }
   }
